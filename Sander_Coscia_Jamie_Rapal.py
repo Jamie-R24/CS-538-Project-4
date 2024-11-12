a4472b549f97910f58fa6d4d3ec813d0aa612707
Jamie-R24 <jamierapal@gmail.com>
2024-11-12 10:21:36 -0500

    5 EDAS completed

--- Sander_Coscia_Jamie_Rapal.py
+++ Sander_Coscia_Jamie_Rapal.py
@@ -8,15 +8,17 @@ import seaborn as sns
 folder_path = "./EDA Graphs/"
 
 df = pd.read_csv('onlineRetail.csv', encoding='ISO-8859-1')
+df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
+df["Month"] = df["InvoiceDate"].dt.month
 
 country_grouped = df.groupby('Country').agg({
-    'Quantity': ['sum', 'mean', 'std'], 
+    'Quantity': ['sum', 'mean', 'std', 'count'], 
     'UnitPrice': ['sum', 'mean', 'std'], 
     'CustomerID': 'nunique'
 }).reset_index()
 
 country_grouped.columns = [ 
-    'Country', 'Total Quantity', 'Average Quantity', 'Quantity Std Dev', 'Total UnitPrice', 'Average UnitPrice', 'UnitPrice Std Dev', 'Unique Customers'
+    'Country', 'Total Quantity', 'Average Quantity', 'Quantity Std Dev', 'Total Count', 'Total UnitPrice', 'Average UnitPrice', 'UnitPrice Std Dev', 'Unique Customers'
 ]
 
 # Plot Total Quantity by Country 
@@ -24,8 +26,15 @@ plt.figure(figsize=(14, 8))
 barplot = sns.barplot(x='Total Quantity', y='Country', data=country_grouped) 
 for bar in barplot.patches: 
     barplot.annotate(f'{bar.get_width():.2f}', (bar.get_width(), bar.get_y() + bar.get_height() / 2), ha='left', va='center')
-plt.savefig(f'{folder_path}Total Quantity by County.png', format='png', dpi = 100)
+plt.savefig(f'{folder_path}Total Quantity by Country.png', format='png', dpi = 100)
 #plt.show() 
+
+# Plot Entries by Country 
+plt.figure(figsize=(14, 8)) 
+barplot = sns.barplot(x='Total Count', y='Country', data=country_grouped) 
+for bar in barplot.patches: 
+    barplot.annotate(f'{bar.get_width():.2f}', (bar.get_width(), bar.get_y() + bar.get_height() / 2), ha='left', va='center')
+plt.savefig(f'{folder_path}Total Count by Country.png', format='png', dpi = 100)
 
 # Plot Average Unit Price by Country 
 plt.figure(figsize=(14, 8))
@@ -37,7 +46,7 @@ for bar in barplot.patches:
 plt.title('Average Unit Price by Country')
 plt.xlabel('Average Unit Price')
 plt.ylabel('Country')
-plt.savefig(f'{folder_path}Average Unit by County.png', format='png', dpi = 100)
+plt.savefig(f'{folder_path}Average Unit by Country.png', format='png', dpi = 100)
 #plt.show()
 
 # Plot Number of Unique Customers by Country 
@@ -50,6 +59,18 @@ for bar in barplot.patches:
 plt.title('Number of Unique Customers by Country')
 plt.xlabel('Unique Customers')
 plt.ylabel('Country')
-plt.savefig(f'{folder_path}Unique Customers by County.png', format='png', dpi = 100)
+plt.savefig(f'{folder_path}Unique Customers by Country.png', format='png', dpi = 100)
 #plt.show()
+
+country_monthly_counts = df.groupby(['Month']).size().reset_index(name = 'Monthly Counts')
+plt.figure(figsize=(14, 8))
+barplot = sns.barplot(x='Month', y='Monthly Counts', data=country_monthly_counts)
+for bar in barplot.patches:
+    barplot.annotate(f'{bar.get_height():.0f}', 
+                     (bar.get_x() + bar.get_width() / 2, bar.get_height()),  
+                     ha='center', va='bottom')  
+plt.title('Invoices by Month')
+plt.xlabel('Month')
+plt.ylabel('Monthly Counts')
+plt.savefig(f'{folder_path}Invoices by Month')
 
